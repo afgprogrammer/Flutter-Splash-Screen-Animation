74f763e8948b3f503754870ecbfb4095180b8200
Mohammad Rahmani <rto1680@gmail.com>
2019-10-03 22:15:26 +0430

    Make login page

---
+++ lib/LoginPage.dart
@@ -0,0 +1,69 @@
+import 'package:day8_splash/Animations/FadeAnimation.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(3, 9, 23, 1),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FadeAnimation(1.2, Text("Login", 
+            style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
+            SizedBox(height: 30,),
+            FadeAnimation(1.5, Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: Colors.grey[300]))
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(color: Colors.grey.withOpacity(.8)),
+                        hintText: "Email or Phone number"
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(color: Colors.grey.withOpacity(.8)),
+                        hintText: "Password"
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+            SizedBox(height: 40,),
+            FadeAnimation(1.8, Center(
+              child: Container(
+                width: 120,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.blue[800]
+                ),
+                child: Center(child: Text("Login", style: TextStyle(color: Colors.white.withOpacity(.7)),)),
+              ),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
